09cc3bca9799e8d49922df59823da80a7c09246c
Arda <aydemirarda10@gmail.com>
2026-01-03 22:05:01 +0100

    Create FAQ system

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             UserSeeder::class,
             NewsArticleSeeder::class,
+            FaqCategorySeeder::class,
+            FaqSeeder::class,
         ]);
     }
 }
