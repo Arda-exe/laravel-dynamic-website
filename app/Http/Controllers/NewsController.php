ed135fd72a7f4efc76ae57240d7a494d1f4d91b7
Arda <aydemirarda10@gmail.com>
2026-01-03 02:27:24 +0100

    Create public NewsController

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NewsArticle;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class NewsController extends Controller
+{
+    /**
+     * Display a listing of news articles.
+     */
+    public function index(): View
+    {
+        $articles = NewsArticle::with('user')
+            ->published()
+            ->latest()
+            ->paginate(10);
+
+        return view('news.index', compact('articles'));
+    }
+
+    /**
+     * Display the specified news article.
+     */
+    public function show(string $slug): View
+    {
+        $article = NewsArticle::with('user')
+            ->where('slug', $slug)
+            ->published()
+            ->firstOrFail();
+
+        return view('news.show', compact('article'));
+    }
+}
