2cab06087a181b320838f9789530981b37a2bc58
Arda <aydemirarda10@gmail.com>
2026-01-04 12:05:52 +0100

    Create forum authorization policies

---
+++ app/Policies/ForumThreadPolicy.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\ForumThread;
+use App\Models\User;
+
+class ForumThreadPolicy
+{
+    /**
+     * Determine if the user can update the thread.
+     */
+    public function update(User $user, ForumThread $thread): bool
+    {
+        return $user->id === $thread->user_id || $user->is_admin;
+    }
+
+    /**
+     * Determine if the user can delete the thread.
+     */
+    public function delete(User $user, ForumThread $thread): bool
+    {
+        return $user->id === $thread->user_id || $user->is_admin;
+    }
+
+    /**
+     * Determine if the user can pin/unpin the thread.
+     */
+    public function pin(User $user): bool
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Determine if the user can lock/unlock the thread.
+     */
+    public function lock(User $user): bool
+    {
+        return $user->is_admin;
+    }
+}
