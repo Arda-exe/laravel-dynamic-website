260aaecaa1b43878f4805115280e560e4ee8c738
Arda <aydemirarda10@gmail.com>
2026-01-08 03:52:32 +0100

    Remove dashboard on login

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -50,6 +50,7 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('home', absolute: false))
+            ->with('success', 'Welcome to the Lands Between, ' . $username . '!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 // Public News Routes
 Route::get('/news', [NewsController::class, 'index'])->name('news.index');
 Route::get('/news/{slug}', [NewsController::class, 'show'])->name('news.show');
